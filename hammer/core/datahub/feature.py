1b63d527b2d1dbe353dca23a9c7e6954202f20f3
liubangbang <lbbagi@yeah.net>
2025-03-25 18:35:13 +0800

    feat: add feature proto definition and implement FeatureComputer with Spark integration

--- hammer/core/datahub/feature.py
+++ hammer/core/datahub/feature.py
@@ -1,21 +1,28 @@
-from sqlalchemy import Column, DateTime, String, UniqueConstraint
+from sqlalchemy import Column, DateTime, Interval, String, UniqueConstraint
 from sqlalchemy.dialects.postgresql import JSON
 
 from ..base import Base
 
 
-class FeatureMeta(Base):
-    __tablename__ = "feature_meta"
+class Feature(Base):
+    __tablename__ = "feature"
 
     name = Column(String(256), nullable=False)
     version = Column(String(256), nullable=False)
-    lineage_id = Column(String(36))
-    parameters = Column(JSON, default=dict)
+    source = Column(JSON)
+    entity = Column(JSON)
+    event_timestamp_field = Column(String(256))
     start_event_datetime = Column(DateTime)
     end_event_datetime = Column(DateTime)
+    ttl = Column(Interval(), nullable=False, default=0)
+    schema = Column(JSON)
+    sink = Column(JSON)
+    transform = Column(String(1024))
+    description = Column(String(256))
+    owner = Column(String(64))
     status = Column(String(20), default="pending")
 
     __table_args__ = (UniqueConstraint("name", "version"),)
 
     def __repr__(self):
-        return f"<FeatureMeta {self.name}:{self.version}>"
+        return f"<Feature {self.name}:{self.version}>"
---
+++ server/hammer-server/src/test/java/me/lbb/FeatureComputer.java
@@ -0,0 +1,20 @@
+package me.lbb;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class FeatureComputer 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
